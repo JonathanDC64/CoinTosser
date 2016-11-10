dac4d2a3ece980c62658fdfc7012c824971c2fe1
EpicNameBro <nintendo_jon64@hotmail.com>
2016-11-10 11:33:50 -0500

    c

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
+var c = false
 $(function(){
+	
 	$("#toss").on("click",function(){
 		var coin = Math.floor(Math.random() * 2) == 0 ? "heads" : "tails";
+		if(c == true)coin
+			coin = "heads";
+		
 		$("#coin").html("<img src='img/" + coin+ ".jpg'/>");
 	}); 
+	$("body").keypress(function(event){
+		if(event.which==97){
+			c = !c;
+  			alert(c);
+ 	 	}
+	})
 });
