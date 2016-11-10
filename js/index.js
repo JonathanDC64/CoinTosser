f7abad661145c151b2a0afff61c12046a24abf7d
EpicNameBro <nintendo_jon64@hotmail.com>
2016-11-10 11:29:00 -0500

    fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(function(){
 	$("#toss").on("click",function(){
 		var coin = Math.floor(Math.random() * 2) == 0 ? "heads" : "tails";
-		$("#coin").html("<img src='" + coin+ ".jpg'/>");
+		$("#coin").html("<img src='img/" + coin+ ".jpg'/>");
 	}); 
 });
